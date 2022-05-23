79975fd6ffa3a72fe1bd46a23387c82f570c054b
brenoslivio <brenoslivio@usp.br>
2022-05-23 11:21:03 -0300

    Only pandas.

--- evaluation_kmers.py
+++ evaluation_kmers.py
@@ -429,20 +429,7 @@ def evaluate_model_cross(classifier, model, finput):
 	types.append(str)
 	column_types = dict(zip(colnames, types))
 
-	n_lines = sum(1 for row in open(finput))
-
-	df = pd.DataFrame(columns=colnames)
-
-	row_loops = 101 # read # lines at a time
-	for i in range(1, n_lines, row_loops): 
-		print(i)
-		data = np.loadtxt(finput, dtype=str, skiprows=i, max_rows = row_loops, delimiter=',')
-		df_new = pd.DataFrame(data, columns=colnames)
-		print(df_new)
-		df = df.append(df_new.astype(column_types), ignore_index=True)
-
-		del df_new
-		del data
+	df = pd.read_csv(finput, names=colnames, dtype=column_types)
 	
 	X = df.iloc[:, 1:-1]
 	y = df.iloc[:, -1]
